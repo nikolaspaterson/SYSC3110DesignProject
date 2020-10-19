3d4f38a67d8f3cfb226fcbeff9741a284dc2bc02
Adi El-Sammak <adielsammak@cmail.carleton.ca>
2020-10-18 23:22:52 -0400

    "Fixed toString()"

--- src/Player.java
+++ src/Player.java
@@ -1,41 +1,98 @@
+package src;
+
 import java.util.HashMap;
 
+/**
+ * The Player class is responsible for containing important attributes that every player should have in the game of Risk.
+ *
+ * @author aelsammak
+ * @author dieuleparfait
+ * @version 1.0
+ */
 public class Player {
 
-    private String player_name;
-    private int troop_count_dist;
-    private HashMap<String,Territory> territories_occupied;
+    private int deployableTroops;
+    private String name;
+    private HashMap<String, Territory> territoriesOccupied;
 
-    public Player(String name){
-        this.territories_occupied = new HashMap<>();
-        this.player_name = name;
-        this.troop_count_dist = 0;
+    /**
+     * Class constructor for the Player class. Sets the name of the player and initializes the HashMap which will store what territory the player occupies.
+     * @param name the name of the player.
+     */
+    public Player(String name) {
+        this.name = name;
+        territoriesOccupied = new HashMap<String, Territory>();
     }
-    public int getTroop_count_dist(){
-        return troop_count_dist;
-    }
-    public void add_troops(int troop_count){
-        this.troop_count_dist += troop_count;
-    }
-    public int place_troop(int troop_count){
-        if(troop_count_dist - troop_count >= 0){
-            troop_count_dist -= troop_count;
-            return troop_count;
-        }
-        else{
-            int temp = troop_count_dist;
-            troop_count_dist = 0;
-            return temp;
-        }
 
+    /**
+     * Getter for the hashmap containing all the territories that the player occupies.
+     * @return HashMap<String,Territory> The territories Occupied.
+     */
+    public HashMap<String, Territory> getTerritoriesOccupied() { return territoriesOccupied; }
+
+    /**
+     * Sets the amount of deployable troops the player can use during their reinforcement.
+     * @param deployableTroops number of deployable troops.
+     */
+    public void setDeployableTroops(int deployableTroops) {
+        this.deployableTroops = deployableTroops;
     }
-    public void add_territory(Territory terr){
-        territories_occupied.put(terr.getName(),terr);
+
+    /**
+     * Gets the amount of deployable troops the player can use during their reinforcement.
+     * @return int number of deployable troops.
+     */
+    public int getDeployableTroops() {
+        return deployableTroops;
     }
-    public String returnName(){
-        return player_name;
+
+    /**
+     * Increments the number of troops in a specified territory.
+     * @param territory the territory where troops will be added.
+     * @param numTroops the number of troops to add.
+     */
+    public void incrementTroops(Territory territory, int numTroops) {
+        territory.setTroops(territory.getTroops() + numTroops);
     }
-    public HashMap<String,Territory> getTerritories_occupied(){
-        return territories_occupied;
+
+    /**
+     * Decrements the number of troops in a specified territory.
+     * @param territory the territory where troops will be removed.
+     * @param numTroops the number of troops to remove.
+     */
+    public void decrementTroops(Territory territory, int numTroops) {
+        territory.setTroops(territory.getTroops() - numTroops);
+    }
+
+    /**
+     * Adds a territory to the HashMap of territories occupied by the player.
+     * @param territoryName the name of the territory.
+     * @param territory the territory object itself.
+     */
+    public void addTerritory(String territoryName, Territory territory) {
+        territoriesOccupied.put(territoryName, territory);
+    }
+
+    /**
+     * Removes a territory from the HashMap of territories occupied by the player.
+     * @param territory the territory to remove.
+     */
+    public void removeTerritory(String territory) {
+        territoriesOccupied.remove(territory);
+    }
+
+    /**
+     * Combines the Player's Name, all their Territory's owned and how many troops are in each Territory.
+     * @return String combination of information above.
+     */
+    public String toString() {
+        String output = "------>Player: " + this.name + "<------\n";
+        for(String str : territoriesOccupied.keySet()) {
+            output += "======Territory Owned======\n";
+            output += "         " + str + "\n";
+            output += "======Troops Owned======\n";
+            output += "            " + territoriesOccupied.get(str).getTroops();
+        }
+        return output + "\n";
     }
 }
--- src/Territory.java
+++ src/Territory.java
@@ -1,43 +1,121 @@
-import java.util.ArrayList;
+package src;
+
 import java.util.HashMap;
 
+/**
+ * The Territory class is responsible for containing all the important attributes of a territory in the game of Risk.
+ *
+ * @author aelsammak
+ * @author dieuleparfait
+ * @version 1.0
+ */
 public class Territory {
 
-    private String territory_name;
-    private String continent_name;
-    private Player player;
+    private Player occupant;
     private int troops;
-    private HashMap<String,Territory> neighbours;
+    private HashMap<String, Territory> neighbours;
+    private String territoryName;
+    private String continentName;
 
-    public Territory(String continent_name, String territory_name){
-        this.continent_name = continent_name;
-        this.territory_name = territory_name;
-        this.neighbours = new HashMap<>();
-        this.troops = 0;
-    }
-    public void set_neighbour(String territory_name, Territory territory_obj){
-        neighbours.put(territory_name,territory_obj);
-    }
-    public void print_info(){
-        System.out.println("Territory Name: " + territory_name );
-        System.out.println("Continent Name: " + continent_name);
-        System.out.println("Neighbours: " + neighbours.keySet().toString());
-        System.out.println("Owner: " + player.returnName());
-        System.out.println("Troop Count: " + troops);
-        System.out.println("================================================");
-    }
-    public void setPlayer(Player new_owner, int troop_count){
-        player = new_owner;
-        troops = troop_count;
-    }
-    public String getName(){
-        return territory_name;
-    }
-    public String getPlayerString(){
-        return player.returnName();
-    }
-    public void setTroops(int troop_count){
-        troops += troop_count;
+    /**
+     * Class constructor for the Territory class. Sets the player who occupies the territory
+     * @param occupant the player who occupies the territory.
+     * @param territoryName the name of the territory.
+     * @param continentName the continent that the territory is in.
+     */
+    public Territory(Player occupant, String territoryName, String continentName) {
+        this.occupant = occupant;
+        this.territoryName = territoryName;
+        this.continentName = continentName;
+        troops = 0;
+        neighbours = new HashMap<>();
     }
 
+    /**
+     * Gets the continent name that this Territory is in.
+     * @return String the continent name.
+     */
+    public String getContinentName() { return continentName; }
+
+    /**
+     * Gets the name of this Territory.
+     * @return String the Territory name.
+     */
+    public String getTerritoryName() { return territoryName; }
+
+    /**
+     * Sets the number of troops in this Territory.
+     * @param troops the number of troops.
+     */
+    public void setTroops(int troops) {
+        this.troops = troops;
+    }
+
+    /**
+     * Gets the number of troops in this Territory.
+     * @return int the number of troops.
+     */
+    public int getTroops() {
+        return troops;
+    }
+
+    /**
+     * Adds a Territory as a neighbour of this Territory into a HashMap.
+     * @param neighbour the neighbouring Territory.
+     */
+    public void addNeighbour(Territory neighbour){ neighbours.put(neighbour.getTerritoryName() ,neighbour); }
+
+    /**
+     * Gets the HashMap of neighbouring Territories for this Territory.
+     * @return HashMap<String,Territory> the HashMap of neighbours.
+     */
+    public HashMap<String,Territory> getNeighbours() {
+        return this.neighbours;
+    }
+
+    /**
+     * Sets a new Player to occupy this Territory.
+     * @param occupant the new Player to Occupy.
+     */
+    public void setOccupant(Player occupant) {
+        this.occupant = occupant;
+    }
+
+    /**
+     * Gets the Player that occupies this Territory.
+     * @return Player the occupant.
+     */
+    public Player getOccupant() {
+        return occupant;
+    }
+
+    /**
+     * This method is used to check if this Territory is neighbours with the @param territoryToCheck.
+     * @param territoryToCheck the territory to check.
+     * @return boolean true if it is a neighbour, false if not.
+     */
+    public boolean isNeighbour(Territory territoryToCheck) {
+        String terrToCheck = territoryToCheck.getTerritoryName();
+        if(this.neighbours.containsKey(terrToCheck)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Combines the name of the Territory and lists all the neighbouring Territories.
+     * @return String combination of the information above.
+     */
+    public String toString() {
+        String output = "------>Territory Name: " + this.territoryName + "<------\n";
+        output += "======Neighbouring Territories======\n";
+
+        for(String str : neighbours.keySet()) {
+            output += "              " + neighbours.get(str) + "\n";
+        }
+
+        output += "==================";
+        return output;
+    }
 }
