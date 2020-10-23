63ff91ee7a66782ccb893f6db2e4430430369e3c
Erik-Iuhas <erikiuhas@cmail.carleton.ca>
2020-10-23 13:07:59 -0400

    Script Update

--- src/Player.java
+++ src/Player.java
@@ -7,12 +7,35 @@ public class Player {
     private HashMap<String,Territory> territories_occupied;
 
     public Player(String name){
+        this.territories_occupied = new HashMap<>();
         this.player_name = name;
         this.troop_count_dist = 0;
     }
-
+    public int getTroop_count_dist(){
+        return troop_count_dist;
+    }
     public void add_troops(int troop_count){
         this.troop_count_dist += troop_count;
     }
+    public int place_troop(int troop_count){
+        if(troop_count_dist - troop_count >= 0){
+            troop_count_dist -= troop_count;
+            return troop_count;
+        }
+        else{
+            int temp = troop_count_dist;
+            troop_count_dist = 0;
+            return temp;
+        }
 
+    }
+    public void add_territory(Territory terr){
+        territories_occupied.put(terr.getName(),terr);
+    }
+    public String returnName(){
+        return player_name;
+    }
+    public HashMap<String,Territory> getTerritories_occupied(){
+        return territories_occupied;
+    }
 }
--- src/Territory.java
+++ src/Territory.java
@@ -5,7 +5,7 @@ public class Territory {
 
     private String territory_name;
     private String continent_name;
-    private String player_name;
+    private Player player;
     private int troops;
     private HashMap<String,Territory> neighbours;
 
@@ -13,15 +13,31 @@ public class Territory {
         this.continent_name = continent_name;
         this.territory_name = territory_name;
         this.neighbours = new HashMap<>();
+        this.troops = 0;
     }
-    public void set_neighbours(String territory_name, Territory territory_obj){
+    public void set_neighbour(String territory_name, Territory territory_obj){
         neighbours.put(territory_name,territory_obj);
     }
     public void print_info(){
         System.out.println("Territory Name: " + territory_name );
         System.out.println("Continent Name: " + continent_name);
         System.out.println("Neighbours: " + neighbours.keySet().toString());
+        System.out.println("Owner: " + player.returnName());
+        System.out.println("Troop Count: " + troops);
         System.out.println("================================================");
+    }
+    public void setPlayer(Player new_owner, int troop_count){
+        player = new_owner;
+        troops = troop_count;
+    }
+    public String getName(){
+        return territory_name;
+    }
+    public String getPlayerString(){
+        return player.returnName();
+    }
+    public void setTroops(int troop_count){
+        troops += troop_count;
     }
 
 }
