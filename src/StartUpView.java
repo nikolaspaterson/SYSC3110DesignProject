c8e4eb9c7a05d1dda6256b6756e73ad85514c467
nikolaspaterson <nikpaterson@cmail.carleton.ca>
2020-10-27 12:21:34 -0400

    made buttons visible
    Signed-off-by: nikolaspaterson <nikpaterson@cmail.carleton.ca>

--- src/StartUpView.java
+++ src/StartUpView.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class StartUpView extends JFrame {
 
@@ -8,49 +10,42 @@ public class StartUpView extends JFrame {
     public static final int HEIGHT = 814;
 
     //declaring buttons, labels, panels and layouts
-    JLabel welcomeLabel = new JLabel("Welcome");
+    JLabel welcomeLabel = new JLabel("Welcome to RISK!");
+    JLabel welcomeTextLabel = new JLabel("Are you ready to conquer the world?");
     JButton playButton = new JButton("Play");
     JButton howToPlayButton = new JButton("How to Play");
     JPanel panel = new JPanel();
     GroupLayout gLayout = new GroupLayout(panel);
 
     public StartUpView(){
-        super("Welcome to RISK");//Sets title of window
+        super("Welcome");//Sets title of window
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //close and stop the program when x is clicked
-        setMinimumSize(new Dimension(WIDTH,HEIGHT)); //set minimum window size
-        setLayout(null); //no layout for window
-        add(panel); //add panel to window
-        panel.setLayout(gLayout); //set panel layout to grouplayout
 
-        gLayout.setAutoCreateGaps(true); //let layout create its own gaps between components
-        gLayout.setAutoCreateContainerGaps(true);
+        panel.setLayout(gLayout); //Set panel layout to GroupLayout
 
-        //grouplayout has 2 dimensions that you declare separately, vertical and horizontal
-        gLayout.setHorizontalGroup( //create horizontal group
-                gLayout.createSequentialGroup() //create a group of components in sequence
-                    .addComponent(welcomeLabel)
-                    .addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.LEADING) //creating a sub group
-                        .addComponent(playButton)
-                        .addComponent(howToPlayButton))
+        gLayout.setHorizontalGroup(gLayout.createSequentialGroup() //Horizontal alignment, giving in sequence
+                .addGap(WIDTH / 5) //leftmost gap
+                .addComponent(playButton)
+                .addGap(WIDTH / 7) //gap between playButton and text above
+                .addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.CENTER) //Subgroup that will be centered with eachother
+                        .addComponent(welcomeLabel)
+                        .addComponent(welcomeTextLabel))
+                .addGap(WIDTH / 7) //gap between text and howToPlayButton
+                .addComponent(howToPlayButton)
         );
 
-        gLayout.setVerticalGroup(
-                gLayout.createSequentialGroup()
-                    .addComponent(welcomeLabel)
+        gLayout.setVerticalGroup(gLayout.createSequentialGroup()
+                .addGap(HEIGHT / 3)
+                .addComponent(welcomeLabel)
+                .addGap(HEIGHT / 20)
+                .addComponent(welcomeTextLabel)
+                .addGap(HEIGHT / 6)
                     .addGroup(gLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                             .addComponent(playButton)
                             .addComponent(howToPlayButton))
         );
 
-/*
-        welcomeLabel.setBounds(WIDTH * (1/3),HEIGHT * (1/3),WIDTH * (2/3),HEIGHT * (1/10));
-
-        panel.add(welcomeLabel);
-
-        add(welcomeLabel);
-        add(playButton);
-        add(howToPlayButton);*/
-
+        setContentPane(panel);
         setSize(WIDTH,HEIGHT); //set window size
         setVisible(true); //make window visible
     }
