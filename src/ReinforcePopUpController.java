e5c0587eea0792ac3796798d4453fc1a0c34f193
Nikolas <47836939+nikolaspaterson@users.noreply.github.com>
2020-10-29 21:12:17 -0400

    added documentation
    Signed-off-by: nikolaspaterson <nikpaterson@cmail.carleton.ca>

--- src/ReinforcePopUpController.java
+++ src/ReinforcePopUpController.java
@@ -1,34 +1,47 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Class that responds to actions performed in ReinforcePopUp.
+ */
 public class ReinforcePopUpController implements ActionListener {
 
     private final ReinforcePopUp popup;
     private final GameEvent ge;
 
+    /**
+     * Controller constructor.
+     * @param reinforcepopup - instance of ReinforcePopUp
+     */
     public ReinforcePopUpController(ReinforcePopUp reinforcepopup){
         this.popup = reinforcepopup;
         ge = new GameEvent(popup.getPlayer());
     }
 
+    /**
+     * Performs an action based on which button triggered the event
+     * @param e ActionEvent
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
+        //Maximum number of troops that can be moved
         int x = Integer.parseInt(popup.getTroops().getText());
+        //number of troops the player want to move
         int deployableTroops = popup.getPlayer().getDeployableTroops();
 
-        if(e.getSource().equals(popup.getPlus())){
+        if(e.getSource().equals(popup.getPlus())){//if plus button is pressed
             if(x < deployableTroops){
                 x++;
             }
             popup.setTroops(x);
-        }else if(e.getSource().equals(popup.getMinus())){
+        }else if(e.getSource().equals(popup.getMinus())){//if minus is pressed
             if(x > 0){
                 x--;
             }
             popup.setTroops(x);
-        }else if(e.getSource().equals(popup.getDeployButton())){
-            if(Integer.parseInt(popup.getTroops().getText()) > 0){
-                ge.reinforce(popup.getTerritory(), Integer.parseInt(popup.getTroops().getText()));
+        }else if(e.getSource().equals(popup.getDeployButton())){ //if deploy button is pressed
+            if(x > 0){
+                ge.reinforce(popup.getTerritory(), x);
                 popup.setVisible(false);
             }
         }
