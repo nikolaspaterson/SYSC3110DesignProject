a778c0bf4e1facb1b5a16793021ccad9a3fc4ab0
Adi El-Sammak <adielsammak@cmail.carleton.ca>
2020-11-02 12:04:13 -0500

    "Attack outcomes are now stored in enums to avoid magic numbers."

--- src/AttackResult.java
+++ src/AttackResult.java
@@ -5,6 +5,8 @@
  * A1D1 - attacker loses 1 troop and defender loses 1 troop
  * D1 - defender loses 1 troop
  * A1 - attacker loses 1 troop
+ *
+ * @author Ahmad El-Sammak
  */
 public enum AttackResult {
     D2, A2, A1D1, D1, A1
